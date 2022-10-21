570183c1c5b0244258fa4c70b7145cc8b487e3c8
Mille Brekke Amundsen <102770182+MilleBA@users.noreply.github.com>
2022-10-21 03:40:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
-var slideIndex = 1;
+
+// Koden for slider
+let slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
@@ -11,9 +13,9 @@ function currentDiv(n) {
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName('mySlides');
-  var dots = document.getElementsByClassName('demo');
+  let i;
+  let x = document.getElementsByClassName('mySlides');
+  let dots = document.getElementsByClassName('demo');
   if (n > x.length) {
     slideIndex = 1;
   }
@@ -30,11 +32,83 @@ function showDivs(n) {
   dots[slideIndex - 1].className += ' w3-red';
 }
 
-function myFunction() {
-  var x = document.getElementById('demo');
-  if (x.className.indexOf('w3-show') == -1) {
-    x.className += ' w3-show';
-  } else {
-    x.className = x.className.replace(' w3-show', '');
+// For automatisk slider
+let myIndex = 0;
+carousel();
+
+function carousel() {
+  let i;
+  let slides = document.getElementsByClassName('mySlides');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
   }
+  myIndex++;
+  if (myIndex > slides.length) {
+    myIndex = 1;
+  }
+  slides[myIndex - 1].style.display = 'block';
+  setTimeout(carousel, 10000);
+}
+
+// Koden for responsive meny (hide på mobile str og show på større skjerm)
+function myFunction(id) {
+  let meny = document.getElementById(id);
+  if (meny.className.indexOf('w3-show') == -1) {
+    meny.className += ' w3-show';
+  } else {
+    meny.className = x.className.replace(' w3-show', '');
+  }
+}
+
+// Koden for å validere form
+function validateFirstName() {
+  let fname = document.forms['myForm']['fName'].value;
+  if (fname == '') {
+    alert('Du må skrive fornavnet ditt!');
+    return false;
+  }
+}
+function validateLastName() {
+  let lname = document.forms['myForm']['lName'].value;
+  if (lname == '') {
+    alert('Du må skrive etternavnet ditt!');
+    return false;
+  }
+}
+function validateAge() {
+  let age = document.forms['myForm']['age'].value;
+  if (age < 0 || age > 100) {
+    alert('Feil alder!');
+    return false;
+  }
+}
+function validateAddress() {
+  let address = document.forms['myForm']['address'].value;
+  if (address == '') {
+    alert('Du må skrive adresse!');
+    return false;
+  }
+}
+function validatePhone() {
+  let phone = document.forms['myForm']['phone'].value;
+  if (phone.length != 8) {
+    alert('Feil mobil nummer!');
+    return false;
+  }
+}
+function validateSms() {
+  let sms = document.forms['myForm']['message'].value;
+  if (sms == '') {
+    alert('Du må skrive melding!');
+    return false;
+  }
+}
+
+function validateForm() {
+  validateFirstName();
+  validateLastName();
+  validateAge();
+  validateAddress();
+  validatePhone();
+  validateSms();
 }
